ac3fbca4b82068e37d38d6899d0f623ff44fa113
Majaahh <maja.blaszczak@proton.me>
2025-06-11 23:15:27 +0200

    a9y18qlte: Init
    Change-Id: If076b9f01ef5887fc3470ee0055a09e3c69d0c62
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ setup-makefiles.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=a9y18qlte
+VENDOR=samsung
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/samsung/a9y18qlte",
+EOF
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+
+# Finish
+write_footers
