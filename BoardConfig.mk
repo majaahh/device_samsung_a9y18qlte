d6247bfd3d42395077f2740a07f263b97b474211
Tim Zimmerman <tim@linux4.de>
2025-08-28 11:28:56 +0200

    a9y18qlte: Define architecture flags
    Change-Id: I2ff5785ba74a2581888f9827908a0335ab3f5c21
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,17 @@
 
 # Inherit proprietary files
 -include vendor/samsung/a9y18qlte/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+# Architecture (Secondary)
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
