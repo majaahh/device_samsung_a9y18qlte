fea95099766021a6dee94ffa10b8df1779091a56
Majaahh <maja.blaszczak@proton.me>
2025-09-21 13:32:57 +0200

    a9y18qlte: Inherit core 64 bit makefile
    Change-Id: I4add88e4cf5cab3c32ab03961897d5cf0142f96c
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- lineage_a9y18qlte.mk
+++ lineage_a9y18qlte.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from a9y18qlte device
 $(call inherit-product, device/samsung/a9y18qlte/device.mk)
 
