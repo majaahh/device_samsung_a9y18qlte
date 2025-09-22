8388cf48be076913ba8d8034e048b277d07eeb58
Majaahh <maja.blaszczak@proton.me>
2025-09-22 12:06:33 +0200

    a9y18qlte: Import MobiCore blobs
    Change-Id: Idf182727b082b3e32d9095bb380679bccd0eeedc
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -52,6 +52,15 @@ if [[ -z "${SRC}" ]]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libMcClient.so |\
+        vendor/lib64/libMcClient.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
