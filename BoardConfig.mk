efc2c3dfd894e6638d57d8ac05d7f7e639c4e008
Tim Zimmerman <tim@linux4.de>
2025-09-23 00:40:20 +0200

    a9y18qlte: Define recovery flags
    Change-Id: I59b189567560ac50d7d2a80be33aa05e4f9a98b3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,11 @@ TARGET_SOC := sdm660
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Recovery
+BOARD_USES_FULL_RECOVERY_IMAGE := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/configs/init/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
